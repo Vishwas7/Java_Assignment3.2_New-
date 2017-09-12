d41f366007ad77a7edc6a1fda23bb113d0b6f5b8
Vishwas7 <svishwas788@gmail.com>
2017-09-12 20:42:22 +0530

    Java3.2

---
+++ src/Sum/Add.java
@@ -0,0 +1,22 @@
+package Sum;
+/**
+ * 
+ * @author Vishwasi
+ * creating a class named Add this class will add two number 
+ * without using + operator.
+ *
+ */
+public class Add {
+	//mai method
+	public static void main(String[] args) {
+		//decalring variables 
+		int GreenApple = 4;
+		int RedApple = 10;
+		//In here setting a logic to add to  numbers without using + operator..
+	    int totalApple = GreenApple-(-RedApple);
+	    // printing Total Numbers of Apple..
+		System.out.println("\nSum of Total Apple : "+ totalApple);
+
+	}
+
+}
